689e34bf5731f1bebba6d6fb3256f87e53dcf16a
River <foxnezz@gmail.com>
2025-01-06 19:41:44 +0400

    [REFACTOR] remove unnecessary stuff

--- kanji_prioritizer.py
+++ kanji_prioritizer.py
@@ -3,9 +3,10 @@ import os
 import urllib.parse
 import re
 import operator
+import types
+import shlex
 
-from functools import reduce
-from datetime import datetime
+from aqt import mw, gui_hooks
 from anki.utils import ids2str
 from aqt.utils import show_info
 
@@ -14,47 +15,38 @@ from aqt.utils import show_info
 
 from . import util, data, config_util
 
-import types
-import shlex
-
-from aqt import mw, gui_hooks
-# from aqt.webview import AnkiWebView
-# from aqt.qt import (QAction, QSizePolicy, QDialog, QHBoxLayout,
-#                     QVBoxLayout, QTabWidget, QLabel, QCheckBox, QSpinBox,
-#                     QComboBox, QPushButton, QLineEdit, Qt, qconnect,
-#                     QScrollArea, QWidget, QMessageBox, QDateTimeEdit,
-#                     QDateTime)
-
-def kanjigrid(mw, config):
-    dids = [config.did]
+def get_units(mw, config):
+    deck_ids = [config.did]
     if config.did == "*":
-        dids = mw.col.decks.all_ids()
-    for deck_id in dids:
+        deck_ids = mw.col.decks.all_ids()
+    
+    for deck_id in deck_ids:
         for _, id_ in mw.col.decks.children(int(deck_id)):
-            dids.append(id_)
-    cids = []
-    #mw.col.find_cards and mw.col.db.list sort differently
-    #mw.col.db.list is kept due to some users being very picky about the order of kanji when using `Sort by: None`
-    if len(config.searchfilter) > 0 and len(config.fieldslist) > 0 and len(dids) > 0:
-        cids = mw.col.find_cards("(" + util.make_query(dids, config.fieldslist) + ") " + config.searchfilter)
+            deck_ids.append(id_)
+    
+    card_ids = []
+    
+    if len(config.searchfilter) > 0 and len(config.fieldslist) > 0 and len(deck_ids) > 0:
+        card_ids = mw.col.find_cards("(" + util.make_query(deck_ids, config.fieldslist) + ") " + config.searchfilter)
     else:
-        cids = mw.col.db.list("select id from cards where did in %s or odid in %s" % (ids2str(dids), ids2str(dids)))
+        card_ids = mw.col.db.list("select id from cards where did in %s or odid in %s" % (ids2str(deck_ids), ids2str(deck_ids)))
 
     units = dict()
     notes = dict()
-    for i in cids:
+    for i in card_ids:
         card = mw.col.get_card(i)
         
-        # tradeoff between branching and mutating here vs collecting all the cards and then filtermapping
         if card.nid not in notes.keys():
             keys = card.note().keys()
             unitKey = set()
             matches = operator.eq
+            
             for keyword in config.fieldslist:
                 for key in keys:
                     if matches(key.lower(), keyword):
                         unitKey.update(set(card.note()[key]))
                         break
+            
             notes[card.nid] = unitKey
         else:
             unitKey = notes[card.nid]
@@ -62,6 +54,7 @@ def kanjigrid(mw, config):
         if unitKey is not None:
             for ch in unitKey:
                 util.addUnitData(units, ch, i, card, config.kanjionly)
+    
     return units
 
 def run_prioritizer():
@@ -71,14 +64,9 @@ def run_prioritizer():
     
     config.did = '*'
     config.fieldslist = ['expression']
-    # config.searchfilter = ''
-    # config.interval = 180
-    # config.groupby = groupby.currentIndex()
     config.sortby = util.SortOrder.NONE
-    # config.lang = pagelang.currentText()
-    # config.unseen = True
     
-    units = kanjigrid(mw, config)
+    units = get_units(mw, config)
     
     unitsList = {
         util.SortOrder.NONE:      sorted(units.values(), key=lambda unit: (unit.idx, unit.count)),
@@ -89,15 +77,15 @@ def run_prioritizer():
         
     total_count = 0
     count_known = 0
+    unknown_units = []
     for unit in unitsList:
-        if unit.count != 0 or config.unseen:
-            total_count += 1
-            bgcolor = util.get_background_color(unit.avg_interval, config.interval, unit.count)
-            if unit.count != 0 or bgcolor not in ["#E62E2E", "#FFF"]:
-                count_known += 1
-            # table += kanjitile(unit.value, bgcolor, total_count, unit.avg_interval)
-            
+        total_count += 1
+        bgcolor = util.get_background_color(unit.avg_interval, config.interval, unit.count)
+        
+        is_known = unit.count != 0 or bgcolor not in ["#E62E2E", "#FFF"]
+        if is_known:
+            count_known += 1
+        else:
+            unknown_units.append(unit)
     
-    # unknown_units = []
-    
-    show_info(f'Total Count: {total_count}, Known: {count_known}')
+    show_info(f'Total Count: {total_count}, Known: {count_known}, Unknown: {len(unknown_units)}')
